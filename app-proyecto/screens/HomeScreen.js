9eab6720301e02bcd6bb0aef5b773157e44f87fd
SebySurfer <sebysurfer@gmail.com>
2024-11-01 19:29:39 -0600

    Test Worked

--- app-proyecto/screens/HomeScreen.js
+++ app-proyecto/screens/HomeScreen.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button} from 'react-native'
 import React from 'react'
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   return (
     <View>
       <Text>HomeScreen</Text>
+    <Button
+    title='Go to Test'
+    onPress={() => navigation.navigate("Test")}
+    />
     </View>
   )
 }
