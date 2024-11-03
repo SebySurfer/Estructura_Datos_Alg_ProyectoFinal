d00fc2b5f74f81d493c873694bcbd893651f8536
SebySurfer <sebysurfer@gmail.com>
2024-11-02 19:40:21 -0600

    Added Hook component

--- app-proyecto/App.js
+++ app-proyecto/App.js
@@ -1,8 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './navigations/StackNavigator.js';
+import StateHook from './components/StateHook.js';
+
+/*
+    <StackNavigator/>
+*/
 
 export default function App() {
   return (
-    <StackNavigator/>
+    <StateHook/>
   )
 }
