4a1d55a8272d23aa42d6dd5c09d00d3870591d41
SebySurfer <sebysurfer@gmail.com>
2024-11-07 13:33:57 -0600

    Criterio_02
    ListaDobleLigada

--- app-proyecto/screens/Criterio_02/ListaDobleLigada.js
+++ app-proyecto/screens/Criterio_02/ListaDobleLigada.js
@@ -1,3 +1,5 @@
+
+/*
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
@@ -8,5 +10,50 @@ export default function ListaDobleLigada() {
     </View>
   )
 }
+*/
+const buckets = require('buckets-js');
+const ListaDeArmas = new buckets.LinkedList();
 
-const styles = StyleSheet.create({})
+const AgregarArma = (arma) => {
+  if(ListaDeArmas.isEmpty() === true){
+    ListaDeArmas.add(arma, 0);
+  } else {
+    const index = ListaDeArmas.size();
+    ListaDeArmas.add(arma, index)
+  }
+}
+
+const MostrarLista = () => {
+  console.log(ListaDeArmas.toArray());
+}
+
+const BorrarArma = (index) =>{
+  if(ListaDeArmas.isEmpty() === true){
+    console.log("Its empty")
+  } else {
+    ListaDeArmas.removeElementAtIndex(index)
+  }
+}
+
+
+
+
+/* 
+Tests: 
+AgregarArma("Misil")
+AgregarArma("Bomba")
+AgregarArma("Misil 2")
+MostrarLista();
+
+BorrarArma(1);
+MostrarLista();
+
+*/
+
+
+
+
+
+
+
+
