229585389e8f6953e7ef26ebf441638259f603c6
SebySurfer <sebysurfer@gmail.com>
2024-11-07 15:31:08 -0600

    Arbol
    No success in displaying it

--- app-proyecto/screens/Criterio_03/Arboles.js
+++ app-proyecto/screens/Criterio_03/Arboles.js
@@ -1,3 +1,5 @@
+
+/*
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
@@ -10,3 +12,55 @@ export default function Arboles() {
 }
 
 const styles = StyleSheet.create({})
+
+
+*/
+
+
+const buckets = require('buckets-js');
+const Arbol  = new buckets.BSTree();
+
+
+const CrearNodo = (elemento) => {
+  if(Arbol.contains(elemento)){
+    console.log("Ya existe")
+  } else {
+    Arbol.add(elemento)
+  }
+}
+
+const BuscarNodo = (elemento) => {
+  if(Arbol.contains(elemento)){
+    console.log("Si lo tiene")
+  } else {
+    console.log("No lo tiene")
+  }
+}
+
+const EliminarNodo = (elemento) => {
+  if(Arbol.contains(elemento)){
+    Arbol.remove(elemento)
+  } else {
+    console.log("No existe el elemento")
+  }
+}
+
+
+const mostrarArbol = () => {
+  console.log(Arbol.toArray())
+}
+
+
+
+CrearNodo(2);
+CrearNodo(7);
+CrearNodo(9);
+CrearNodo(1);
+CrearNodo(4);
+CrearNodo(2);
+
+mostrarArbol();
+
+
+
+
