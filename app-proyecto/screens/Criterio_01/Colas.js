914c67bef3be2319ca46cf6e6d3d78de14b68de4
SebySurfer <sebysurfer@gmail.com>
2024-11-23 06:19:34 -0600

    Start

--- app-proyecto/screens/Criterio_01/Colas.js
+++ app-proyecto/screens/Criterio_01/Colas.js
@@ -1,111 +1,112 @@
-
-
-/*
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, Button, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+const buckets = require('buckets-js');
 
 export default function Colas() {
+  const [listaDeAviones, setListaDeAviones] = useState([
+    ["B-2", 3],
+    ["F-14", 4],
+    ["F-5 Tiger II", 7],
+    ["F-16", 5],
+    ["MiG-21", 2],
+  ]);
+
+  const [colaDeAviones, setColaDeAviones] = useState(new buckets.Queue());
+
+  const updateAvionCount = (index, avionName) => {
+    setListaDeAviones((prevList) =>
+      prevList.map((avion, i) =>
+        i === index && avion[1] > 0 ? [avion[0], avion[1] - 1] : avion
+      )
+    );
+    const newCola = new buckets.Queue(colaDeAviones.toArray());
+    newCola.add(avionName);
+    setColaDeAviones(newCola);
+  };
+
+  const despegarAvion = () => {
+    const newCola = new buckets.Queue(colaDeAviones.toArray());
+    newCola.dequeue(); // Remove the first airplane
+    setColaDeAviones(newCola);
+  };
+
   return (
-    <View>
-      <Text>Colas</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Lista de Aviones</Text>
+
+        {/* Display the list of airplanes */}
+        <FlatList
+          data={listaDeAviones}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemText}>
+                {item[0]} - Count: {item[1]}
+              </Text>
+              <Button
+                title={`Add ${item[0]}`}
+                onPress={() => updateAvionCount(index, item[0])}
+                disabled={item[1] === 0}
+              />
+            </View>
+          )}
+        />
+
+        {/* Display the queue */}
+        <Text style={styles.title}>Cola de Aviones</Text>
+        {colaDeAviones.size() > 0 ? (
+          colaDeAviones.toArray().map((avion, index) => (
+            <Text key={index} style={styles.queueItem}>
+              {index + 1}. {avion}
+            </Text>
+          ))
+        ) : (
+          <Text style={styles.emptyQueue}>La cola está vacía</Text>
+        )}
+
+        {/* Dequeue button */}
+        <Button title="Despegar Avión" onPress={despegarAvion} disabled={colaDeAviones.size() === 0} />
+      </View>
+    </SafeAreaView>
+  );
 }
 
- */
-
-let listaDeAviones = [
-  ["B-2", 3], 
-  ["F-14", 4], 
-  ["F-5 Tiger II", 7], 
-  ["F-16", 5], 
-  ["MiG-21", 2]
-  ];
-
-const getB2 = () => {
-  if(listaDeAviones[0][1] !== 0){
-    listaDeAviones[0][1] -= 1;
-    ColaDeAviones.add("B-2")
-  }
-}
-
-const getF14 = () =>{
-  if(listaDeAviones[1][1] !== 0){
-    listaDeAviones[1][1] -= 1;
-    ColaDeAviones.add("F-14")
-  }
-
-}
-
-const getF5 = () =>{
-  if(listaDeAviones[2][1] !== 0){
-    listaDeAviones[2][1] -= 1;
-    ColaDeAviones.add("F-5 Tiger II")
-  }
-
-}
-
-const getF16 = () =>{
-  if(listaDeAviones[3][1] !== 0){
-    listaDeAviones[3][1] -= 1;
-    ColaDeAviones.add("F-16")
-  }
-
-}
-
-  
-const getMig = () =>{
-  if(listaDeAviones[4][1] !== 0){
-    listaDeAviones[4][1] -= 1;
-    ColaDeAviones.add("MiG-21")
-  }
-
-}
-
-const buckets = require('buckets-js');
-const ColaDeAviones  = new buckets.Queue();
-
-ColaDeAviones.add("F-14")
-ColaDeAviones.add("F-16")
-ColaDeAviones.add("B-2")
-ColaDeAviones.add("MiG-21")
-
-
-const Despegar = () =>{
-  ColaDeAviones.dequeue();
-}
-
-const printLista = () => {
-  console.log(ColaDeAviones.toArray());
-}
-
-
-/*
-Tests:
-
-printLista();
-getMig();
-getMig();
-
-getB2();
-getB2();
-
-console.table(listaDeAviones)
-printLista();
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#f9f9f9",
+  },
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  itemContainer: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    elevation: 3,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  itemText: {
+    fontSize: 16,
+  },
+  queueItem: {
+    fontSize: 16,
+    paddingVertical: 5,
+  },
+  emptyQueue: {
+    fontSize: 16,
+    color: "gray",
+    fontStyle: "italic",
+    marginTop: 10,
+  },
+});
